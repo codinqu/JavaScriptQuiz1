dd663a98f6eb7bce3af95377912233d2e5342381
codinqu <Bianca.McKinley@mscoding.org>
2024-08-02 11:43:45 -0700

    updated and finished java quiz
    quiz is complete. i think or at least hope i did pretty good. (:

---
+++ script.js
@@ -0,0 +1,66 @@
+//question one
+
+let fruits = ["apple", "banana", "cherry"];
+let cher = (fruits[2]);
+console.log(cher);
+
+
+//question two
+
+let numbers = ["1", "2", "3", "4", "5"];
+
+let sum = (numbers[2] + numbers[4]);
+
+console.log(sum);
+
+
+
+//question three
+
+let languages = ["JavaScript", "Python", "Java"];
+
+languages.pop = (languages[2]);
+
+languages.push = ("Ruby");
+
+console.log(languages);
+
+
+//question four
+
+let age = "20";
+
+if (age >= 18){
+    console.log("Wow you are old! That means you are eligable to vote!");
+}
+else if (age <= 18){
+    console.log("Haha you are not eligable to vote yet!");
+}
+else {
+    console.log("Do you even exist bro?");
+};
+
+
+// Question five
+
+let cities = ["New York", "Los Angeles", "Chicago"];
+let lastCity = (cities[2]);
+
+console.log(lastCity);
+
+
+//question six
+
+let x = "7";
+
+if (x > 5){
+    console.log("Soldier, you are in range! Clear! Over!");
+}
+else if (x < 10){
+    console.log("Soldier, you are out of range. Over.");
+}
+else {
+    console.log("Soldier down. Affrimitive.");
+};
+
+//quiz finished 
